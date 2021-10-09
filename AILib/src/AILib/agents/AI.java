e20d58c787131ad7e9172f766d3b2bc813a08f6f
OwnMind-ai <75088081+OwnMind-ai@users.noreply.github.com>
2021-10-09 10:09:13 +0300

    Commented Agent.java and AI.java

--- AILib/src/AILib/agents/AI.java
+++ AILib/src/AILib/agents/AI.java
@@ -3,43 +3,50 @@ package AILib.agents;
 import AILib.layers.InputLayer;
 import AILib.layers.Layer;
 import AILib.layers.Layers;
-import AILib.layers.StaticLayer;
 import AILib.utills.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 
-public class AI implements Agent{
-    public static double VERSION = 1.1d;
-    public static double LAYER_SPLITTER = -256;
-    public static double WEIGHTS_START = -257;
+/*
+* NeuralNetwork is base class to neural network agents,
+* has neural structure and can 
+*/
 
-    protected ArrayList<Layer> layers;     //Dynamic array of Neurons classes
-    public float fault = 0.0005f;          //Minimal error of neural network1
+public class AI implements Agent{    //TODO: rename AI to NeuralNetwork
+    public static double VERSION = 1.1d;         // Version of saving format, not used in program
+    public static double LAYER_SPLITTER = -256;  // Code of start layer's structure description
+    public static double WEIGHTS_START = -257;   // Code of start neural network weights enumeration
 
-    public AI(int inputNeurons){    //Initialization by pre-creating first layer         //Dynamic ID
+    protected ArrayList<Layer> layers;     // Dynamic array of Neurons classes
+    public float fault = 0.0005f;          // Minimal error of neural network
+
+    public AI(int inputNeurons){    //Initialization by pre-creating first layer
         buildAI(inputNeurons);
     }
 
-    public AI(String fileName){                          //Initialization by import of existing AI
-        double[] AIParameters = FileHandler.readFile(fileName);       //AI's file data
+    // TODO: rename AIParameters to parameters
+    public AI(String fileName){                          //Initialization by import Agent save file
+        double[] AIParameters = FileHandler.readFile(fileName);       // Gets data from save file
         assert(AIParameters != null);
         this.layers = new ArrayList<>();
 
-        int i = 2;
+        int i = 2;          // Starts from first Layer index(skip version and layer splitter)
+        // This code in if statement add InputLayer to neural network without calling Layer::buildLayer
         if(AIParameters[i] == Arrays.asList(Layers.values()).indexOf(Layers.INPUT_LAYER)){
             this.layers.add(
                     Layers.values()[(int) AIParameters[i]].getInstance(
                         new double[]{AIParameters[i + 1]}
             ));
-            i+= 2;
+            i+= 2;   // Set index at next layer splitter
         }
         while(AIParameters[i] != AI.WEIGHTS_START){
-            //Initialization layers
+            // Initialization layers
             if(AIParameters[i] == AI.LAYER_SPLITTER){
+                // Gets layer data after layer index
                 double[] data = new double[Layers.values()[(int) AIParameters[i + 1]].getDataLength()];
                 System.arraycopy(AIParameters, i + 2, data, 0, data.length);
+                // Adds layer by getting instance from Layers Enum
                 this.addLayer(
                         Layers.values()[(int) AIParameters[i + 1]].getInstance(data)
                 );
@@ -47,12 +54,13 @@ public class AI implements Agent{
             i+= 1;
         }
 
+        // Getting weights from AIParameters
         double[] weights = new double[(int) (AIParameters.length - i - 1)];
         System.arraycopy(AIParameters, i + 1, weights, 0, weights.length);
         this.setWeights(weights);     //Setting weights from file data
     }
 
-    private void buildAI(int neuronsCount){    //Building AI structure
+    private void buildAI(int neuronsCount){    // Building Agent base structure
         this.layers = new ArrayList<>();
         this.layers.add(new InputLayer(neuronsCount));
     }
@@ -61,32 +69,36 @@ public class AI implements Agent{
         this.fault = fault;
     }
 
-    public void addLayer(Layer layer) {              //Adds layer to [this.layers] array and adds neurons to it
-        this.layers.add(layer);                     //Adding layer
+    public void addLayer(Layer layer) {              // Adds layer to [this.layers] array and adds neurons to it
+        this.layers.add(layer);
+        // Builds layer by giving information of previous layers
         this.layers.get(this.layers.size() - 1).buildLayer(
                 this.layers.get(this.layers.size() - 2).getNeuronsLength()
         );
     }
 
-    public void addAll(Layer... layers){
+    public void addAll(Layer... layers){    // Adds several layers at once
         for(Layer layer : layers)
             this.addLayer(layer);
     }
 
-    public double[] start(double[] inputData){      //Runs the input array through neural network and returns output
-        this.layers.get(0).setOutputs(inputData);
+    public double[] start(double[] inputData){      // Runs the input array through neural network and returns output
+        this.layers.get(0).setOutputs(inputData);   // Loads data to first layer
 
         for(int i = 1; i < this.layers.size(); i++)
+            // Does layer by giving result on previous layer
             this.layers.get(i).doLayer(this.layers.get(i - 1).getOutputs());
 
+        // Returns outputs of last layer as Agent output
         return this.layers.get(this.layers.size() - 1).getOutputs();
     }
 
-    public int[] AIChecker(double[][][] example, int roundRate){   //Compare dataset array and AI output
-        int[] resultsInfo = {0, 0};             //resultInfo[0] - dataset length, resultInfo[1] - AI and dataset matches
+    // TODO: mov AIChecker to external class
+    public int[] AIChecker(double[][][] example, int roundRate){   // Compare dataset array and AI output
+        int[] resultsInfo = {0, 0};             // resultInfo[0] - dataset length, resultInfo[1] - AI and dataset matches
         for(double[][] dataset : example){
             resultsInfo[0]++;
-            double[] result = this.start(dataset[0]);   //Contains AI's output. Will be casted to float[] answer soon
+            double[] result = this.start(dataset[0]);   // Contains AI's output. Will be casted to float[] answer soon
             float[] answer = new float[result.length];
             for(int i = 0; i < answer.length; i++)
                 answer[i] = (float) ((float) Math.round(result[i] * Math.pow(10, roundRate)) / Math.pow(10, roundRate));
@@ -134,28 +146,34 @@ public class AI implements Agent{
                     this.layers.get(i).getNeuron(a).setWeight(b, weights[index++]);
     }
 
+    // TODO: rename AIParametersList and AIParameters to parametersList and parameters
     public void save(String fileName){    //Writes AI(structure, weights) to following filepath
         //Cast three-dimensional [this.dataset] array to one-dimensional array
         double[] weights = Arrays.stream(this.getWeights())
                 .flatMap(Arrays::stream).flatMapToDouble(Arrays::stream).toArray();
-        ArrayList<Double> AIParametersList = new ArrayList<>();
-        AIParametersList.add(AI.VERSION);
+
+        ArrayList<Double> AIParametersList = new ArrayList<>();  // List of parameters that will saved
+        AIParametersList.add(AI.VERSION);                        // Adds saving format version
         for (Layer layer : this.layers) {
+            // Adds splitter and starts write Layer structure to list
             AIParametersList.add(AI.LAYER_SPLITTER);
 
+            // Adds index of layer from Layers Enum
             AIParametersList.add((double) Layers.getLayerID(layer.getClass()));
-            double[] data = layer.getArchivedData();
+            double[] data = layer.getArchivedData();   // Gets data of layer structure
             for (double d : data)
                 AIParametersList.add(d);
         }
+        // Starts to adding 1D weights array to list
         AIParametersList.add(AI.WEIGHTS_START);
 
+        // Cast parameters list to doubles array
         double[] AIParameters = AIParametersList.stream().mapToDouble(Double::doubleValue).toArray();
         //Merging [AIParameters] and AI's weights arrays
         double[] result = Arrays.copyOf(AIParameters, weights.length + AIParameters.length);
         System.arraycopy(weights, 0, result, AIParameters.length, weights.length);
 
-        FileHandler.writeToFile(result, fileName);
+        FileHandler.writeToFile(result, fileName);  // Writes Agent data to file
     }
 
     //Saving AI without arguments to unique filepath
--- AILib/src/AILib/agents/Agent.java
+++ AILib/src/AILib/agents/Agent.java
@@ -2,12 +2,17 @@ package AILib.agents;
 
 import AILib.layers.Layer;
 
+/*
+*  Classes that implement the Agent interface are often neural networks,
+*  can process data and save themselves to a file
+*/
+
 public interface Agent {
-    double[] start(double[] input);
+    double[] start(double[] input);   // Process input data
 
-    void addLayer(Layer layer);
-    void addAll(Layer... layers);
+    void addLayer(Layer layer);       // Adds Layer to neural network
+    void addAll(Layer... layers);     // Equals to Agent::addLayer(Layer layer), but can add several layers
 
-    void save(String fileName);
-    void save();
+    void save(String fileName);       // Saves Agent structure and data to file
+    void save();                      // Equals to Agent::save(String fileName), but filepath is instance hashcode
 }
