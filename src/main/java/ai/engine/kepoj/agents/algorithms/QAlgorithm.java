78e444f59f8de9983d11c5e9af6b71ed5047ad0d
OwnMind-ai <ownmind39@gmail.com>
2022-06-26 20:57:48 -0600

    Extending Agent interface by algorithm interfaces

--- src/main/java/ai/engine/kepoj/agents/algorithms/QAlgorithm.java
+++ src/main/java/ai/engine/kepoj/agents/algorithms/QAlgorithm.java
@@ -1,11 +1,13 @@
 package ai.engine.kepoj.agents.algorithms;
 
+import ai.engine.kepoj.agents.Agent;
+
 /**
- * Points to a class that implements Q-learning algorithm
+ * Points to an agent that implements Q-learning algorithm
  *
  * @since 1.2
  * @see ai.engine.kepoj.agents.deep.QNeuralNetwork
  */
-public interface QAlgorithm{
+public interface QAlgorithm extends Agent {
     void learningIteration(double reward, double[] nextState, double discountFactor, double ratio);
 }
--- src/main/java/ai/engine/kepoj/agents/algorithms/SupervisedAlgorithm.java
+++ src/main/java/ai/engine/kepoj/agents/algorithms/SupervisedAlgorithm.java
@@ -1,14 +1,15 @@
 package ai.engine.kepoj.agents.algorithms;
 
+import ai.engine.kepoj.agents.Agent;
 import ai.engine.kepoj.entities.Dataset;
 
 /**
- * Points to a class that implements learning by provided dataset.
+ * Points to an agent that implements learning by provided dataset.
  * Training realized in two forms - by the minimal error and by ages count
  * @since 1.2
  * @see ai.engine.kepoj.agents.deep.SupervisedNeuralNetwork
  */
-public interface SupervisedAlgorithm {
+public interface SupervisedAlgorithm extends Agent {
     double train(Dataset dataset, double ratio, double fault);
 
     double train(Dataset dataset, double ratio, long ages);
