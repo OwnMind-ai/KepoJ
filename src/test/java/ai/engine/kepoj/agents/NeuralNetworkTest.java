4391101c54e3ca72d8b102782dd456ba75c17b3d
OwnMind-ai <ownmind39@gmail.com>
2022-09-24 23:15:56 -0600

    Remove usage of deprecated methods in tests

--- src/test/java/ai/engine/kepoj/agents/NeuralNetworkTest.java
+++ src/test/java/ai/engine/kepoj/agents/NeuralNetworkTest.java
@@ -1,6 +1,7 @@
 package ai.engine.kepoj.agents;
 
 import ai.engine.kepoj.entities.Dataset;
+import ai.engine.kepoj.entities.Neuron;
 import ai.engine.kepoj.functions.StandardFunctions;
 import ai.engine.kepoj.agents.deep.NeuralNetwork;
 import ai.engine.kepoj.layers.StaticLayer;
@@ -18,10 +19,23 @@ class NeuralNetworkTest {
         NeuralNetwork network = new NeuralNetwork(4);
         network.addLayer(new StaticLayer(4, StandardFunctions.THRESHOLD));
         network.addLayer(new StaticLayer(2, StandardFunctions.THRESHOLD));
-        network.setWeights(new double[]{-1,-1,0,0,-1, 1,1,0,0,-1, 0,0,-1,-1,-1, 0,0,1,1,-1,
+        setWeights(network, new double[]{-1,-1,0,0,-1, 1,1,0,0,-1, 0,0,-1,-1,-1, 0,0,1,1,-1,
                                          1,0,0,1,-1, 0,1,1,0,-1});
 
         for (double[][] data : dataset.toArray()) assertArrayEquals(data[1], network.react(data[0]));
+    }
+
+    public void setWeights(NeuralNetwork network, double[] weights) {
+        int index = 0;
+        for(int i = 1; i < network.getLayers().size(); i++)
+            for(int a = 0; a < network.getLayers().get(i).length(); a++)
+                for(int b = 0; b < network.getLayers().get(i).getNeuron(a).weights.size() + 1; b++)
+                    setWeightForNeuron(network.getLayers().get(i).getNeuron(a), b, weights[index++]);
+    }
+
+    public void setWeightForNeuron(Neuron neuron, int index, double value){
+        if(index == neuron.weights.size()) neuron.bias = value;
+        else neuron.weights.set(index, value);
     }
 
     @Test
